658f26be54823696a2e095df44812741ce8a952a
Shane Connell <s1a9c73@aol.co.uk>
2025-05-18 14:21:14 +0100

    Remove duplicate index route from run.py

--- run.py
+++ run.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template
-from app.routes import main  
+from flask import Flask
+from app.routes import main
 
-app = Flask(__name__)
-app.register_blueprint(main)   
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+app = Flask(__name__, template_folder='app/templates')
+app.register_blueprint(main)
 
 if __name__ == '__main__':
     app.run(debug=True)
