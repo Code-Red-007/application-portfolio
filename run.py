38c4b5e92938d139fa81be755918be4c171405b7
Shane Connell <s1a9c73@aol.co.uk>
2025-05-21 18:52:23 +0100

    Fix: defined static folder path for Render

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from app.routes import main
 
-app = Flask(__name__, template_folder='app/templates')
+app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 app.register_blueprint(main)
 
 if __name__ == '__main__':
